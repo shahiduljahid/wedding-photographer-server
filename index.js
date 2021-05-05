0e83448159d3e80a66c18af4c3ce1a6789886223
shahidulJahid <shahiduljahid3@gmail.com>
2021-05-05 10:32:58 +0800

    sij

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
 const port = 4000;
 
 
@@ -27,6 +28,7 @@ const client = new MongoClient(
 client.connect((err) => {
   console.log("database connected");
   const weddingCollection = client.db("weddings").collection("appointments");
+  const adminCollection = client.db("weddings").collection("admins");
 
   app.post("/addappointment", (req, res) => {
     const appointment = req.body;
@@ -40,6 +42,43 @@ client.connect((err) => {
         console.log(err);
       });
   });
+  
+  app.post("/addAdmin", (req, res) => {
+    const admin = req.body;
+    console.log(admin);
+    adminCollection
+      .insertOne(admin)
+      .then((result) => {
+        res.send(result.insertedCount > 0);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  app.post("/admin", (req, res) => {
+    const admin = req.body.admin;
+    console.log(admin);
+    adminCollection.find({admin:admin}).toArray((err,documents)=>{
+      res.send(documents.length>0)
+    })
+      
+     
+     
+  });
+
+  app.patch('/updateStatus',(req,res)=>{
+    const id = req.body.bookingId ;
+    const status = req.body.status;
+    weddingCollection.updateOne({_id: ObjectId(id)},
+    { $set: { status:status } })
+    .then((result) => {
+      res.send(result.modifiedCount>0);
+      
+      
+    });
+
+  })
   //   app.post("/appointments", (req, res) => {
   //     const date = req.body;
   //     console.log(date.date);

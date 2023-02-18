818c4638b2e91959de1de2ba871068b791916fdf
shahiduljahid3 <shahiduljahid3@gmail.com>
2023-02-18 08:08:49 +0800

    refactor done

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ require("dotenv").config();
 
 const uri = process.env.DB_URI;
 
-
-
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -29,6 +27,8 @@ client.connect((err) => {
   console.log("database connected");
   const weddingCollection = client.db("weddings").collection("appointments");
   const adminCollection = client.db("weddings").collection("admins");
+  const serviceCollection = client.db("weddings").collection("services");
+  const reviewCollection = client.db("weddings").collection("reviews");
 
   app.post("/addappointment", (req, res) => {
     const appointment = req.body;
@@ -42,7 +42,7 @@ client.connect((err) => {
         console.log(err);
       });
   });
-  
+
   app.post("/addAdmin", (req, res) => {
     const admin = req.body;
     console.log(admin);
@@ -59,38 +59,62 @@ client.connect((err) => {
   app.post("/admin", (req, res) => {
     const admin = req.body.admin;
     console.log(admin);
-    adminCollection.find({admin:admin}).toArray((err,documents)=>{
-      res.send(documents.length>0)
-    })
-      
-     
-     
+    adminCollection.find({ admin: admin }).toArray((err, documents) => {
+      res.send(documents.length > 0);
+    });
   });
 
-  app.patch('/updateStatus',(req,res)=>{
-    const id = req.body.bookingId ;
+  app.patch("/updateStatus", (req, res) => {
+    const id = req.body.bookingId;
     const status = req.body.status;
-    weddingCollection.updateOne({_id: ObjectId(id)},
-    { $set: { status:status } })
-    .then((result) => {
-      res.send(result.modifiedCount>0);
-      
-      
-    });
+    weddingCollection
+      .updateOne({ _id: ObjectId(id) }, { $set: { status: status } })
+      .then((result) => {
+        res.send(result.modifiedCount > 0);
+      });
+  });
 
-  })
-  //   app.post("/appointments", (req, res) => {
-  //     const date = req.body;
-  //     console.log(date.date);
-  //     appointmentCollection
-  //       .find({ date: date.date })
-  //       .toArray((err, documents) => {
-  //         res.send(documents);
-  //         console.log(documents);
-  //       });
-  //   });
   app.get("/totalAppointments", (req, res) => {
     weddingCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  app.get("/services", (req, res) => {
+    serviceCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  app.post("/userService", (req, res) => {
+    const user = req.body.user;
+
+    weddingCollection.find({ user: user }).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  app.post("/addService", (req, res) => {
+    const service = req.body;
+    serviceCollection
+      .insertOne(service)
+      .then((result) => {
+        res.send(result.insertedCount > 0);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  app.post("/addReview", (req, res) => {
+    const review = req.body;
+    reviewCollection
+      .insertOne(review)
+      .then((result) => {
+        res.send(result.insertedCount > 0);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  app.get("/reviews", (req, res) => {
+    reviewCollection.find({}).toArray((err, documents) => {
       res.send(documents);
     });
   });
